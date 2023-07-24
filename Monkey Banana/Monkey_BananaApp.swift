8b62be036c5ec559b3559b5b26cceae6067233d3
AbdElRahman Rafaat Amer <abdelrahman.raafaat@gmail.com>
2023-07-24 20:33:39 +0300

    Initial Commit

---
+++ Monkey Banana/Monkey_BananaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monkey_BananaApp.swift
+//  Monkey Banana
+//
+//  Created by Abdelrahman Rafaat on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Monkey_BananaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Monkey BananaUITests/Monkey_BananaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Monkey_BananaUITestsLaunchTests.swift
+//  Monkey BananaUITests
+//
+//  Created by Abdelrahman Rafaat on 24/07/2023.
+//
+
+import XCTest
+
+final class Monkey_BananaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
